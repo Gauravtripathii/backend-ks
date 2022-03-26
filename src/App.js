1a91f11bf2fee04792e36e99d156cbd8e14c2439
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-03-26 15:17:07 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
+import CA_SignUp from "./components/CA_SignUp";
 
 function App() {
   return (
     <div className="app">
       <h1>Hello, There!!</h1>
+      <CA_SignUp />
     </div>
   );
 }
---
+++ src/components/CA_SignUp.js
@@ -0,0 +1,9 @@
+function CA_SignUp() {
+  return (
+    <form action="/">
+      <input type="text" />
+    </form>
+  );
+}
+
+export default CA_SignUp;
