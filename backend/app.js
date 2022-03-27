c6333ba97d1c29180dca36c8515b547830301fe2
VPS07 <vikysingh07860@gmail.com>
2022-03-27 18:16:16 +0530

    add form submit function

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World");
+// });
+
+app.post("/signup", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(port, () => {
