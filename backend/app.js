526a9b7ce0675a907d6c92abe02536334b9ebcf0
Gauravtripathii <gauravtripathii7979@gmail.com>
2022-03-27 10:46:16 +0530

    basic setup | (use npm start) to run app | express is now serving react app

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  // res.send("Hello World");
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
